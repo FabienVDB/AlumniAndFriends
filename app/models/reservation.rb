d451889da540ca9b38b9569ee9b64a5bea46234f
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-05-24 18:20:01 +0200

    Validate models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,12 +2,17 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :alumni, class_name: "User"
 
-  validates :alumni, if: :alumni?
+  validate :alumni_must_be_alumni
   validates :location, presence: true
   validates :date, presence: true
+  validates :date, date: true
+  validates :date, date: { after: proc { Time.now } }
+
   validates :duration, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def alumni?
-    alumni.alumni
+  def alumni_must_be_alumni
+    return if alumni.alumni
+
+    errors.add(:alumni, "must be an alumni")
   end
 end
