be33255b1d7a3a7adc01f3cece397a0c4c1d1c3a
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-05-25 21:38:46 +0200

    Add roots
    reservation route

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,7 @@
 class ReservationsController < ApplicationController
+  def index
+    # @reservations = Reservation.where(user_id: params[:user_id])
+    @user = User.find(params[:id])
+    @reservations = user.reservations
+  end
 end
