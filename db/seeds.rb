f1d8dc2929e5c33920fdce56518f93353b090383
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-05-25 12:24:54 +0200

    Basic user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroy users"
+User.destroy_all if Rails.env.development?
+puts "destroy reservations"
+Reservation.destroy_all if Rails.env.development?
+puts "destroy review"
+Review.destroy_all if Rails.env.development?
+
+User.create!(email: "fabvdb@hotmail.com",
+             password: "fabien_secret",
+             alumni: true,
+             first_name: "Fabien",
+             last_name: "VAN DEN BERG",
+             phone_number: "0645210239",
+             description: "Je peux t'aider à coder en Ruby")
+
+User.create!(email: "zanounekalid@.com",
+             password: "kalid_secret",
+             alumni: true,
+             first_name: "Kalid",
+             last_name: "ZANOUNE",
+             phone_number: "0623154496",
+             description: "Je peux être un super buddy")
+
+User.create!(email: "herezy2006@gmail.com",
+             password: "kevin_secret",
+             alumni: true,
+             first_name: "Kevin",
+             last_name: "HANG",
+             phone_number: "0659763106",
+             description: "Je peux donner des cours de surf et de natation")
+
+User.create!(email: "alain.verse@gmail.com",
+             password: "alain_secret",
+             first_name: "Alain",
+             last_name: "VERSE",
+             phone_number: "0545785241",
+             description: "J'ai besoin d'aider pour coder en ruby")
