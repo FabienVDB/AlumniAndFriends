5c3343f072c0305dd10fa29306f8d6e3121f4d74
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-05-27 01:18:32 +0200

    Create Review form - TODO : star rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  def create
+    comment = params[:review][:comment]
+    rating = params[:review][:rating]
+    reservation = Reservation.find(params[:reservation_id])
+    client = User.find(params[:user_id])
+    @review = Review.new(user: client, reservation: reservation, comment: comment, rating: rating)
+    @review.save
+    redirect_to user_reservations_path(current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
-    resources :reservations, only: %i[index create]
+    resources :reservations, only: %i[index create] do
+      resources :reviews, only: [:create]
+    end
   end
 
   resources :reservations, only: :destroy
