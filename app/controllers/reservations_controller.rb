e4aad303913df5652f02fdddab7eb44be054b39c
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-05-26 18:16:26 +0200

    Add reservation routes and #reservations#create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,18 @@ class ReservationsController < ApplicationController
   def index
     # @reservations = Reservation.where(user_id: params[:user_id])
     @user = User.find(params[:user_id])
-    @reservations = @user.reservations
+    @next_reservations = @user.reservations.where('date >= ?', DateTime.now)
+    @past_reservations = @user.reservations.where('date < ?', DateTime.now)
+  end
+
+  def create
+    @reservation = Reservation.new(client: current_user,
+                                   provider: User.find(params[:user_id]),
+                                   date: params['reservation']['date'])
+    if @reservation.save
+      redirect_to user_reservations_path(@reservation.client)
+    else
+      redirect_to user_path(@reservation.provider)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show] do
-    resources :reservations, only: :index
+  resources :users, only: %i[index show] do
+    resources :reservations, only: %i[index create]
   end
 end
