f95f606ed13ebaa652c04c4176db729d47ffe9e3
Kevin Hang <herezy2006@gmail.com>
2022-05-26 16:19:26 +0200

    datepickr

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,16 +1,2 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
-
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
-
-var post= document.getElementById("post");
-post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("comment-box").value;
-
-    var li = document.createElement("li");
-    var text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
-
-});
