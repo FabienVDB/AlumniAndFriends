1316b10f20a6a58d68c6aee9e6cc90edda7a38c5
zanoune kalid <zanounekalid@hotmail.fr>
2022-05-27 13:09:02 +0200

    finStimulus

---
+++ app/javascript/controllers/comment_controller.js.js
@@ -0,0 +1,13 @@
+
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "displayed" ]
+  // connect() {
+  //   console.log("Hello from our first Stimulus controller")
+  // }
+  display(event){
+      event.preventDefault()
+      this.displayedTarget.classList.toggle("d-none")
+    }
+}
