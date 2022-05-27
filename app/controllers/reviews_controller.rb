b7c2e9ec66c00c919da249f8bce74858f9018fcb
Kevin Hang <herezy2006@gmail.com>
2022-05-27 13:27:03 +0200

    nothing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,10 +11,4 @@ class ReviewsController < ApplicationController
       redirect_to user_reservations_path(current_user)
     end
   end
-
-  def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
-    redirect_to user_path(current_user)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
       resources :reviews, only: [:create]
     end
   end
-  resources :reviews, only: :destroy
   resources :reservations, only: :destroy
 end
